1db406da06d9be3cb91d91d67318fb266eec0559
woojh3690 <woojh3690@gmail.com>
2022-08-23 13:47:18 +0900

    executable jar 빌드 환경 설정

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,8 +1,8 @@
 public class Main {
     public static void main(String[] args) {
-//        MyUnit myUnit = new MyUnit();
+        MyUnit myUnit = new MyUnit();
         System.out.println("Program start");
-//        System.out.println(myUnit.concatenate("first", "second"));
+        System.out.println(myUnit.concatenate("Woo,", "Jun-Hyeok"));
         System.out.println("Program end");
     }
 }
